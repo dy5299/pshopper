32748187ab13b65dba992704cc3a79e201ac1a22
dy5299 <dykgit@gmail.com>
2020-05-21 11:04:02 +0900

    keras 제외하고 원복

--- model/tensorflow-triplet-loss/model/model_fn.py
+++ model/tensorflow-triplet-loss/model/model_fn.py
@@ -61,11 +61,11 @@ def model_fn(features, labels, mode, params):
 
     # -----------------------------------------------------------
     # MODEL: define the layers of the model
-    with tf.compat.v1.variable_scope('model'):
+    with tf.variable_scope('model'):
         # Compute the embeddings with the model
         embeddings = build_model(is_training, images, params)
     embedding_mean_norm = tf.reduce_mean(tf.norm(embeddings, axis=1))
-    tf.compat.v1.summary.scalar("embedding_mean_norm", embedding_mean_norm)
+    tf.summary.scalar("embedding_mean_norm", embedding_mean_norm)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {'embeddings': embeddings}
@@ -88,7 +88,7 @@ def model_fn(features, labels, mode, params):
     # Metrics for evaluation using tf.metrics (average over whole dataset)
     # TODO: some other metrics like rank-1 accuracy?
     with tf.variable_scope("metrics"):
-        eval_metric_ops = {"embedding_mean_norm": tf.compat.v1.metrics.mean(embedding_mean_norm)}
+        eval_metric_ops = {"embedding_mean_norm": tf.metrics.mean(embedding_mean_norm)}
 
         if params.triplet_strategy == "batch_all":
             eval_metric_ops['fraction_positive_triplets'] = tf.metrics.mean(fraction)
@@ -105,8 +105,8 @@ def model_fn(features, labels, mode, params):
     tf.summary.image('train_image', images, max_outputs=1)
 
     # Define training step that minimizes the loss with the Adam optimizer
-    optimizer = tf.compat.v1.train.AdamOptimizer(params.learning_rate)
-    global_step = tf.compat.v1.train.get_global_step()
+    optimizer = tf.train.AdamOptimizer(params.learning_rate)
+    global_step = tf.train.get_global_step()
     if params.use_batch_norm:
         # Add a dependency to update the moving mean and variance for batch normalization
         with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
